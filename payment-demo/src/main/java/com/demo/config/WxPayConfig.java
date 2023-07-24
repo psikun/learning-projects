c33728fe91d6fc00eab717a5562b1853ec4ca824
psikun <psikun@foxmail.com>
2023-07-24 14:50:39 +0800

    feat: 新增payment-demo微信配置文件

--- payment-demo/src/main/java/com/demo/config/WxPayConfig.java
+++ payment-demo/src/main/java/com/demo/config/WxPayConfig.java
@@ -1,15 +1,23 @@
 package com.demo.config;
 
 
+import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
+import com.wechat.pay.contrib.apache.httpclient.auth.PrivateKeySigner;
+import com.wechat.pay.contrib.apache.httpclient.auth.ScheduledUpdateCertificatesVerifier;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Credentials;
+import com.wechat.pay.contrib.apache.httpclient.auth.WechatPay2Validator;
 import com.wechat.pay.contrib.apache.httpclient.util.PemUtil;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 import java.security.PrivateKey;
 
 
@@ -56,4 +64,43 @@ public class WxPayConfig {
 
     }
 
+    /**
+     * 获取签名认证器
+     *
+     * @return
+     */
+    @Bean
+    public ScheduledUpdateCertificatesVerifier getVerifier() {
+
+        // 获取商户私钥
+        PrivateKey privateKey = getPrivateKey(privateKeyPath);
+
+        // 私钥签名对象
+        PrivateKeySigner privateKeySigner = new PrivateKeySigner(mchSerialNo, privateKey);
+
+        // 微信认证对象
+        WechatPay2Credentials wechatPay2Credentials = new WechatPay2Credentials(mchId, privateKeySigner);
+
+        // 使用定时更新的签名验签器，不需要传入证书
+        ScheduledUpdateCertificatesVerifier verifier = new ScheduledUpdateCertificatesVerifier(
+                wechatPay2Credentials, apiV3Key.getBytes(StandardCharsets.UTF_8));
+        return verifier;
+    }
+
+    @Bean
+    public CloseableHttpClient getWxPayClient(ScheduledUpdateCertificatesVerifier verifier) {
+
+        // 获取商户私钥
+        PrivateKey privateKey = getPrivateKey(privateKeyPath);
+
+
+        WechatPayHttpClientBuilder builder = WechatPayHttpClientBuilder.create()
+                .withMerchant(mchId, mchSerialNo, privateKey)
+                .withValidator(new WechatPay2Validator(verifier));
+
+        // 通过WechatPayHttpClientBuilder构造的ttpClient,会自动的处理签名和验签，并进行证书自动更新
+        CloseableHttpClient httpClient = builder.build();
+
+        return httpClient;
+    }
 }
